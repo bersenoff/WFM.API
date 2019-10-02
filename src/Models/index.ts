2161447889e27b35a8fd3c4cf389409fc032868f
Берсенёв Никита Дмитриевич <bersenoff@bk.ru>
2019-10-02 19:16:50 +0300

    добавил настройки пула

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -4,6 +4,12 @@ export default new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER,
   host: process.env.MYSQL_HOST,
   port: Number(process.env.MYSQL_PORT),
   dialect: "mysql",
+  pool: {
+    max: 40,
+    min: 0,
+    idle: 10000,
+    acquire: 30000
+  },
   timezone: "+03:00",
   logging: false,
 });
